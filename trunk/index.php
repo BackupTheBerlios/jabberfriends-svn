da8906d7ba617208efe714d4c84d261443786307
inputmice <inputmice@ef6a2d0e-0c0b-0410-bb4b-cab0dead92c2>
2006-01-27 14:13:40 +0000

    a new favicon

--- trunk/index.php
+++ trunk/index.php
@@ -2,33 +2,33 @@
 <div xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
     <head>
         <title>JabberFriends.org :: All Around The World</title>
-            <style type="text/css">
-                body{
-                    text-align: center;
-                    background-color: #4b72b0;
-                    color: #c7e0ff;
-                    font-size: 20pt;
-                    font-family: Verdana, Arial, sans-serif;
-                }
-                a {
-                    color: #c7e0ff;
-                    font-size: 18pt;
-                    font-family: Verdana, Arial, sans-serif;
-                    font-weight: bold;
-                    text-decoration: none;
-                }
-                h1 {
-                    color: #c7e0ff;
-                    font-size: 25pt;
-                    font-family: Verdana, Arial, sans-serif;
-                    font-weight: bold;
-                    text-decoration: none;
-                }
-            </style>
-            <meta name="description" content="{META_DESCRIPTION}" />
-            <meta name="auther" content="{META_AUTHER}" />
-
-            <meta name="keywords" content="{META_KEYWORDS}" />
+        <link rel="shortcut icon" href="/favicon.ico">
+        <style type="text/css">
+            body{
+                text-align: center;
+                background-color: #4b72b0;
+                color: #c7e0ff;
+                font-size: 20pt;
+                font-family: Verdana, Arial, sans-serif;
+            }
+            a {
+                color: #c7e0ff;
+                font-size: 18pt;
+                font-family: Verdana, Arial, sans-serif;
+                font-weight: bold;
+                text-decoration: none;
+            }
+            h1 {
+                color: #c7e0ff;
+                font-size: 25pt;
+                font-family: Verdana, Arial, sans-serif;
+                font-weight: bold;
+                text-decoration: none;
+            }
+        </style>
+        <meta name="description" content="{META_DESCRIPTION}" />
+        <meta name="auther" content="{META_AUTHER}" />
+        <meta name="keywords" content="{META_KEYWORDS}" />
     </head>
     <body>
         <h1>JabberFriends.org - All Around The World</h1>
