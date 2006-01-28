367b5c3c984910361857f920522fe61d6a0c5c72
inputmice <inputmice@ef6a2d0e-0c0b-0410-bb4b-cab0dead92c2>
2006-01-28 16:37:10 +0000

    you can change your profile

---
+++ trunk/changedetails.php
@@ -0,0 +1,97 @@
+<?php
+include('includes/config.php');
+include('classes/jforg_template.php');
+include('classes/jforg_user.php');
+$user = new jforg_user();
+if (in_array($_GET['lang'],$config['languages'])) {
+    $language = $_GET['lang'];
+} else {
+    die('Language ist nicht bekannt');
+}
+$template = new jforg_template();
+$user = new jforg_user();
+$template->set_path('design');
+$template->set_frame('formpage','green');
+$template->hover_on('green');
+SESSION_START();
+$user_id = $user->get_id($_SESSION['nick']);
+if (!$user->login($_SESSION['nick'],$_SESSION['passwd'])) {
+    die('You are not logged in');  
+}
+if ($_POST['submit']!="") {
+    $_POST['birthdate'] = $birthdate_year.'-'.$birthdate_month.'-'.$birthdate_date;
+    $user->set_details($user_id,$_POST);
+}
+$user_details = $user->get_details($user_id);
+$brithvar = $user_details['BIRTHDATE']{8}.$user_details['BIRTHDATE']{9};
+$birthdate_date_select = "";
+for($i=01;$i<=31; $i++) {
+    if ($i==$brithvar) {
+        $birthdate_date_select = "$birthdate_date_select<option value=\"$i\" selected>$i</option>";
+    } else {
+        $birthdate_date_select = "$birthdate_date_select<option value=\"$i\" >$i</option>";
+    }
+}
+$yearvar = $user_details['BIRTHDATE']{0}.$user_details['BIRTHDATE']{1}.$user_details['BIRTHDATE']{2}.$user_details['BIRTHDATE']{3};
+$birthdate_year_select = "";
+for($i=date('Y')-80;$i<=date('Y'); $i++) {
+    if ($i==$yearvar) {
+        $birthdate_year_select = "$birthdate_year_select<option value=\"$i\" selected>$i</option>";
+    } else {
+        $birthdate_year_select = "$birthdate_year_select<option value=\"$i\" >$i</option>";
+    }
+}
+$monthvar = $user_details['BIRTHDATE']{5}.$user_details['BIRTHDATE']{6};
+if ($monthvar==1) { $birthdate_month_select = "$birthdate_month_select<option value=\"1\" selected>January</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"1\">January</option>"; }
+if ($monthvar==2) { $birthdate_month_select = "$birthdate_month_select<option value=\"2\" selected>February</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"2\">February</option>"; }
+if ($monthvar==3) { $birthdate_month_select = "$birthdate_month_select<option value=\"3\" selected>March</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"3\">March</option>"; }
+if ($monthvar==4) { $birthdate_month_select = "$birthdate_month_select<option value=\"4\" selected>April</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"4\">April</option>"; }
+if ($monthvar==5) { $birthdate_month_select = "$birthdate_month_select<option value=\"5\" selected>May</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"5\">May</option>"; }
+if ($monthvar==6) { $birthdate_month_select = "$birthdate_month_select<option value=\"6\" selected>June</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"6\">June</option>"; }
+if ($monthvar==7) { $birthdate_month_select = "$birthdate_month_select<option value=\"7\" selected>July</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"7\">July</option>"; }
+if ($monthvar==8) { $birthdate_month_select = "$birthdate_month_select<option value=\"8\" selected>August</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"8\">August</option>"; }
+if ($monthvar==9) { $birthdate_month_select = "$birthdate_month_select<option value=\"9\" selected>September</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"9\">September</option>"; }
+if ($monthvar==10) { $birthdate_month_select = "$birthdate_month_select<option value=\"10\" selected>October</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"10\">October</option>"; }
+if ($monthvar==11) { $birthdate_month_select = "$birthdate_month_select<option value=\"11\" selected>November</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"11\">November</option>"; }
+if ($monthvar==12) { $birthdate_month_select = "$birthdate_month_select<option value=\"12\" selected>Dezember</option>"; } else { $birthdate_month_select = "$birthdate_month_select<option value=\"12\">Dezember</option>"; }
+$content = $content.'<tr><td colspan="2"><a href="{LINK_OPTIONS}">{LANG_BACK_TO_OPTIONS}</a><br /><br /></td></tr>';
+$content = $content.'<tr><td class="left">{LANG_NICK}</td><td class="right">'.$user->get_nick($user_id).'</td></tr>';
+$content = $content.'<tr><td>{LANG_JID}</td><td>'.$user->get_jid($user_id).'</td></tr>';
+$content = $content.'<tr><td colspan="2"><br /><b>{LANG_ABOUT_YOU}</b></td></tr>';
+$content = $content.$realname_warning;
+$content = $content.'<tr><td>{LANG_REALNAME}</td><td><input type="text" name="realname" value="';
+if ($_POST['realname']=="") { $content = $content.$user_details['REALNAME']; } else { $content = $content.$_POST['realname']; }
+$content = $content.'" /></td></tr>';
+$content = $content.'<tr><td>{LANG_BIRTHDATE}</td><td><select name="birthdate_date">'.$birthdate_date_select.'</select> <select name="birthdate_month">'.$birthdate_month_select.'</select> <select name="birthdate_year">'.$birthdate_year_select.'</select></td></tr>';
+$content = $content.'<tr><td>{LANG_COUNTRY}</td><td><input type="text" name="country" value="'.$user_details['COUNTRY'].'" /></td></tr>';
+$content = $content.'<tr><td>{LANG_CITY}</td><td><input type="text" name="city" value="'.$user_details['CITY'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_ORIGINAL_FROM}</td><td><input type="text" name="original_from" value="'.$user_details['ORIGINAL_FROM'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_LANGUAGES}</td><td><input type="text" name="languages" value="'.$user_details['LANGUAGES'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_HOBBYS}</td><td><input type="text" name="hobbys" value="'.$user_details['HOBBYS'].'" /></td></tr>';
+$content = $content.'<tr><td colspan="2"><br /><b>{LANG_YOUR_COMPUTER}</b></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_COMPUTER}</td><td><input type="text" name="computer" value="'.$user_details['COMPUTER'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_COMPUTER_OS}</td><td><input type="text" name="computer_os" value="'.$user_details['COMPUTER_OS'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_GEEKCODE}</td><td><textarea cols="200" rows="3" name="geekcode">'.$user_details['GEEKCODE'].'</textarea></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_PUBLICKEY}</td><td><textarea cols="200" rows="8" name="publickey">'.$user_details['PUBLICKEY'].'</textarea></td></tr>';
+$content = $content.'<tr><td colspan="2"><br /><b>{LANG_YOUR_FAVORITES}</b></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_FAVORITE_FILM}</td><td><input type="text" name="favorite_film" value="'.$user_details['FAVORITE_FILM'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_FAVORITE_SERIES}</td><td><input type="text" name="favorite_series" value="'.$user_details['FAVORITE_SERIES'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_FAVORITE_MUSIK}</td><td><input type="text" name="favorite_musik" value="'.$user_details['FAVORITE_MUSIK'].'" /></td></tr>';
+$content = $content.'<tr><td valign="top">{LANG_FAVORITE_BOOK}</td><td><input type="text" name="favorite_book" value="'.$user_details['FAVORITE_BOOK'].'" /></td></tr>';
+$content = $content.'<tr><td>&nbsp;</td><td><br /><input class="submit" value="{LANG_CHANGE}" name="submit" type="submit" /></td></tr>';
+$template->replace('FULLPAGE_TEXT',$content);
+$template->replace('LOGIN','{LANG_LOGOUT}');
+$template->replace('REGISTER','{LANG_OPTIONS}');
+$template->replace('LINK_LOGIN','{LINK_LOGOUT}');
+$template->replace('LINK_REGISTER','{LINK_OPTIONS}');
+$template->replace('LOGIN','{LANG_LOGIN}');
+$template->replace('REGISTER','{LANG_REGISTER}');
+$template->replace('FORM_ACTION','');
+$template->replace('LINK_GERMAN','/de/angaben_aendern.htm');
+$template->replace('LINK_ENGLISH','/en/change_details.htm');
+$template->replace('META_TITLE','{LANG_CHANGEDETAILS}');
+$template->replace('FULLPAGE_HEADER','{LANG_CHANGEDETAILS}');
+$template->translate($language);
+include('includes/links.php');
+$template->write();
+?>
--- trunk/user_details.php
+++ trunk/user_details.php
@@ -30,7 +30,7 @@ if ($user->login($_SESSION['nick'],$_SESSION['passwd'])) {
 $user_details = $user->get_details($user_id);
 $content = $content.'<tr><td valign="top">{LANG_NICK}</td><td valign="top">'.$user->get_nick($user_id).'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_JID}</td><td valign="top">'.$user->get_jid($user_id).'</td></tr>';
-$content = $content.'<tr><td colspan="2"><b>{LANG_ABOUT} '.$user->get_nick($user_id).'</b></td></tr>';
+$content = $content.'<tr><td colspan="2"><br /><b>{LANG_ABOUT} '.$user->get_nick($user_id).'</b></td></tr>';
 $content = $content.'<tr><td valign="top" class="left">{LANG_REALNAME}</td><td valign="top" class="right">'.$user_details['REALNAME'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_BIRTHDATE}</td><td valign="top">'.$user_details['BIRTHDATE'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_COUNTRY}</td><td valign="top">'.$user_details['COUNTRY'].'</td></tr>';
@@ -38,19 +38,19 @@ $content = $content.'<tr><td valign="top">{LANG_CITY}</td><td valign="top">'.$us
 $content = $content.'<tr><td valign="top">{LANG_ORIGINAL_FROM}</td><td valign="top">'.$user_details['ORIGINAL_FROM'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_LANGUAGES}</td><td valign="top">'.$user_details['LANGUAGES'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_HOBBYS}</td><td valign="top">'.$user_details['HOBBYS'].'</td></tr>';
-$content = $content.'<tr><td colspan="2"><b>{LANG_COMPUTER_OF} '.$user->get_nick($user_id).'</b></td></tr>';
+$content = $content.'<tr><td colspan="2"><br /><b>{LANG_COMPUTER_OF} '.$user->get_nick($user_id).'</b></td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_COMPUTER}</td><td valign="top">'.$user_details['COMPUTER'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_COMPUTER_OS}</td><td valign="top">'.$user_details['COMPUTER_OS'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_GEEKCODE}</td><td valign="top">'.$user_details['GEEKCODE'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_PUBLICKEY}</td><td valign="top">';
 if ($user_details['PUBLICKEY']!="") { $content = $content.'<a href="/'.$language.'/publickeys/'.$user_id.'-'.$user->get_nick($user_id).'.htm">{LANG_SHOW_PUBLICKEY}</a>'; }
 $content = $content.'</td></tr>';
-$content = $content.'<tr><td colspan="2"><b>{LANG_FAVORITES} '.$user->get_nick($user_id).'</b></td></tr>';
+$content = $content.'<tr><td colspan="2"><br /><b>{LANG_FAVORITES} '.$user->get_nick($user_id).'</b></td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_FAVORITE_FILM}</td><td valign="top">'.$user_details['FAVORITE_FILM'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_FAVORITE_SERIES}</td><td valign="top">'.$user_details['FAVORITE_SERIES'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_FAVORITE_MUSIK}</td><td valign="top">'.$user_details['FAVORITE_MUSIK'].'</td></tr>';
 $content = $content.'<tr><td valign="top">{LANG_FAVORITE_BOOK}</td><td valign="top">'.$user_details['FAVORITE_BOOK'].'</td></tr>';
-$content = $content.'<tr><td colspan="2"><b>Tags</b></td></tr>';
+$content = $content.'<tr><td colspan="2"><br /><b>Tags</b></td></tr>';
 $template->replace('FULLPAGE_HEADER','{LANG_USER_PAGE_OF} '.$user->get_nick($user_id));
 $template->replace('META_TITLE','{LANG_USER_PAGE_OF} '.$user->get_nick($user_id));
 $template->replace('FULLPAGE_TEXT',$content);
