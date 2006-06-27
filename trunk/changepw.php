0715ebe38108d697ed72bd87af7aee7f948c0f5b
inputmice <inputmice@ef6a2d0e-0c0b-0410-bb4b-cab0dead92c2>
2006-06-27 14:00:22 +0000

    change a bug in changepw.php an add a complete mysql dump

--- trunk/changepw.php
+++ trunk/changepw.php
@@ -28,27 +28,37 @@ $content .= '<form action="{FORM_ACTION}" method="post">
                 <table cellpadding="0" cellspacing="2" border="0">';
 
 if (!empty($_POST['submit'])) {
-    $succes = $user->login($_SESSION['nick'], mysql_real_escape_string($_POST['old_pw']) );
-    echo $_SESSION['nick'];
-    echo $_POST['old_pw'];
-    if ($succes == true){
-        if($_POST['new_pw'] == $_POST['new_pw2']){ 
-            if($user->change_password($_SESSION['nick'], $_POST['new_pw'])){
-                $content .='<tr><td></td><td>{LANG_CHANGEPW_SUCCES}</td></tr>';
-                $_SESSION['passwd'] = $_POST['new_pw'];
-            }
-        }else{
-            $content .='<tr><td></td><td>{LANG_PW_NOT_SAME}</td></tr>';
-        }
-    }else{
-    $content .='<tr><td></td><td>{LANG_PWUSER}!</td></tr>';
+    $counter = 0;
+    //Stimmt das alte PW
+    if ($user->login($_SESSION['nick'],$_POST['old_pw'])) {
+        $counter++;
+    } else {
+        $pwold_comment = '<tr><td>&nbsp;</td><td><em>{LANG_PWWRONG}</em></td></tr>';
     }
-
+    //Stimmen die PWs überein
+    if ($_POST['new_pw']==$_POST['new_pw2']) {
+        $counter++;
+    } else {
+        $pw_comment = '<tr><td>&nbsp;</td><td><em>{LANG_PW_NOT_SAME}</em></td></tr>';
+    }
+    //Vailides PW?
+    if (preg_match('/[-._+\d\w]{6,}/i',$_POST['new_pw'])) {
+        $counter++;
+    } else {
+        $pw_comment = '<tr><td>&nbsp;</td><td><em>{LANG_ONLY_LETTERS_6}</em></td></tr>';
+    }
+    if ($counter==3) {
+        if($user->change_password($_SESSION['nick'], $_POST['new_pw'])){
+            $pwold_comment = '<tr><td></td><td><em>{LANG_CHANGEPW_SUCCES}</em></td></tr>';
+            $_SESSION['passwd'] = $_POST['new_pw'];
+        }
+    }
 }
 
-
-$content .= '<tr><td>{LANG_OLD_PW}</td><td><input type="text" name="old_pw" value=""></td>';
-$content .= '<tr><td>{LANG_NEW_PW}</td><td><input type="text" name="new_pw" value=""></td></tr><tr><td>{LANG_PW_AGAIN}</td><td><input type="text" name="new_pw2" value=""></td></tr><tr><td>&nbsp;</td><td><br /><input class="submit" value="{LANG_CHANGE_PW}" name="submit" type="submit" /></td></tr></table></form>';
+$content .= $pwold_comment;
+$content .= '<tr><td>{LANG_OLD_PW}</td><td><input type="password" name="old_pw" value=""></td>';
+$content .= $pw_comment;
+$content .= '<tr><td>{LANG_NEW_PW}</td><td><input type="password" name="new_pw" value=""></td></tr><tr><td>{LANG_PW_AGAIN}</td><td><input type="password" name="new_pw2" value=""></td></tr><tr><td>&nbsp;</td><td><br /><input class="submit" value="{LANG_CHANGE_PW}" name="submit" type="submit" /></td></tr></table></form>';
 
 $template->replace('FULLPAGE_TEXT',$content);
 $template->replace('LOGIN','{LANG_LOGIN}');
