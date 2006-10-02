80c1588c156b767c22f2d5af4df672f1cda66cf6
blase16 <blase16@ef6a2d0e-0c0b-0410-bb4b-cab0dead92c2>
2006-10-02 14:01:34 +0000

    Fixed the user link in publickey.php

--- trunk/includes/members/publickey.php
+++ trunk/includes/members/publickey.php
@@ -12,7 +12,7 @@ if ($language=="de") {
 if ($language=="en") {
     $link = "members";
 }
-$content = '<a href="/'.$language.'/'.$link.'/'.$user_id.'-'.$user->get_nick($user_id).'.htm">{LANG_SHOW_USER_PAGE_OF} '.$user->get_nick($user_id).'</a><br /><br />';
+$content = '<a href="/'.$language.'/'.$link.'/'.$user->get_nick($user_id).'.htm">{LANG_SHOW_USER_PAGE_OF} '.$user->get_nick($user_id).'</a><br /><br />';
 $content = $content.str_replace("\n","<br />",$user_details['PUBLICKEY']);
 $template->replace('FULLPAGE_TEXT',$content);
 $template->replace('FULLPAGE_HEADER','{LANG_PUBLICKEY_OF} '.$user->get_nick($user_id));
