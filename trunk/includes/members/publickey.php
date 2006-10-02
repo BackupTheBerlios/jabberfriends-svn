e51a97427cd3789df12e932255405e3e097dc726
blase16 <blase16@ef6a2d0e-0c0b-0410-bb4b-cab0dead92c2>
2006-10-02 15:03:25 +0000

    Added no such key to publickey.php

--- trunk/includes/members/publickey.php
+++ trunk/includes/members/publickey.php
@@ -1,6 +1,10 @@
 <?php
-$user_id = $user->get_id($_GET['id']);
 $template->set_frame('fullpage','green');
+if (!$user->nick_exists($_GET['id'])) {
+    $content = 'Such user and public key doesn\'t exists';
+    $template->replace('FULLPAGE_HEADER','There is no '.$_GET['id']);
+}else{
+$user_id = $user->get_id($_GET['id']);
 $user_details = $user->get_details($user_id);
 if ($language=="de") {
     $link = "mitglieder";
@@ -10,8 +14,9 @@ if ($language=="en") {
 }
 $content = '<a href="/'.$language.'/'.$link.'/'.$user->get_nick($user_id).'.htm">{LANG_SHOW_USER_PAGE_OF} '.$user->get_nick($user_id).'</a><br /><br />';
 $content = $content.str_replace("\n","<br />",$user_details['PUBLICKEY']);
-$template->replace('FULLPAGE_TEXT',$content);
 $template->replace('FULLPAGE_HEADER','{LANG_PUBLICKEY_OF} '.$user->get_nick($user_id));
+}
+$template->replace('FULLPAGE_TEXT',$content);
 $template->replace('META_TITLE','{LANG_PUBLICKEY_OF} '.$user->get_nick($user_id));
 $template->replace('LINK_GERMAN','/de/publickeys/'.$user->get_nick($user_id));
 $template->replace('LINK_ENGLISH','/en/publickeys/'.$user->get_nick($user_id));
